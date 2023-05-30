fb2420ecaa5b4219ad4e74a7ab27cef2aead699f
Yakuma Kuramy <104542682+YakumaKuramy@users.noreply.github.com>
2023-05-30 16:51:15 -0300

    update
    questão sobre conversão de temperaturas

--- FUP/Conversor_de_C_para_F.py
+++ FUP/Conversor_de_C_para_F.py
@@ -1,3 +1,4 @@
 # Implemente um programa que recebe a temperatura em graus Celsius e converte para Fahrenheit. 
 c = float(input("Informe a temperatura em Celcius: "))
 f = ((c / 5) * 9 ) + 32
+print("A temperatura é {} que equivale a {}".format(c, f))
